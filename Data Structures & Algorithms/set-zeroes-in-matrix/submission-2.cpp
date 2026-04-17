b572352e38b09dbc39aaf251a34e816d0df3c35f
Sanassi Cissé <65661724+sanassi@users.noreply.github.com>
2026-04-17 10:48:48 +0200

    Add: set-zeroes-in-matrix - submission-2

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+   public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        const int m = matrix.size();
+        const int n = matrix[0].size();
+
+
+        std::set<std::pair<int, int>> zeroes;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0)
+                    zeroes.insert({i, j});
+            }
+        }
+
+        for (const auto &zero : zeroes) {
+            //int x = zero.first;
+            //int y = zero.second;
+
+            for (int i = 0; i < n; i++) {
+                matrix[zero.first][i] = 0;
+            }
+
+            for (int i = 0; i < m; i++) {
+                matrix[i][zero.second] = 0;
+            }
+        }
+    }
+};
