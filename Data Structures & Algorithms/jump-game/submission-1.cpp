b9fa906c884647f2b53431e61df9a6254318fee1
Sanassi Cissé <65661724+sanassi@users.noreply.github.com>
2026-05-05 16:59:48 +0200

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    bool rec(std::vector<int> &nums, int index) {
+        if (index >= nums.size() - 1) {
+            return true;
+        }
+
+        //std::cout << nums.size() << ", " << index << "\n";
+
+        for (int jump = 1; jump <= nums[index]; jump++) {
+            bool res = rec(nums, index + jump);
+            if (res) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool canJump(vector<int>& nums) {
+        return rec(nums, 0);
+    }
+};
