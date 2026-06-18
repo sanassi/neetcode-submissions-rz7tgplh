4729e9c6cf612bf1728b9c3c5b5040147516bf9f
Sanassi Cissé <65661724+sanassi@users.noreply.github.com>
2026-06-18 14:25:29 +0200

    Add: contains-duplicate-ii - submission-4

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-4.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < nums.size(); j++) {
+                if (i != j && std::abs(i - j) <= k && nums[i] == nums[j])
+                    return true;
+                if (std::abs(i - j) > k)
+                    continue;
+            }
+        }
+        return false;
+    }
+};
