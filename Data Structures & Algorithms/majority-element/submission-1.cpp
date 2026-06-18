487a58610cc6f123fec99fe425a5450e3d42d98f
Sanassi Cissé <65661724+sanassi@users.noreply.github.com>
2026-06-18 11:53:07 +0200

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size(); i++) {
+            int cnt = 0;
+            int val = nums[i];
+
+            while (i < nums.size() && nums[i] == val) {
+                i++;
+                cnt++;
+
+                if (cnt > std::floor(nums.size() / 2))
+                    return val;
+            }
+
+            i -= 1;
+
+            std::cout << val << " " << cnt << std::endl;
+        }
+
+        return -42;
+    }
+};
