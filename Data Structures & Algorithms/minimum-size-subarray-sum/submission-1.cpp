b8f2723676f4f5bd0f9f554b38294f091a2ead05
Sanassi Cissé <65661724+sanassi@users.noreply.github.com>
2026-06-19 09:43:02 +0200

    Add: minimum-size-subarray-sum - submission-1

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    // returns the max sum of subarrays of size k
+    int slidingSum(int target, std::vector<int> &nums, int k) {
+        int sum = 0;
+        int max_sum = 0;
+
+        for (int i = 0; i < k; i++)
+            sum += nums[i];
+        
+        max_sum = sum;
+
+        for (int i = k; i < nums.size(); i++) {
+            sum += nums[i] - nums[i - k];
+            max_sum = std::max(max_sum, sum);
+        }
+
+        return max_sum;
+    }
+
+    // process increasing subarray len, return the first match
+    int minSubArrayLen(int target, vector<int>& nums) {
+        for (int k = 1; k <= nums.size(); k++) {
+            if (slidingSum(target, nums, k) >= target)
+                return k;
+        }
+
+        return 0;
+    }
+};
